b1e0e464f0919e636de73dc847b74503d0ecfa70
YARINSWINDOWSPC\yarin <yarinmagdaci@gmail.com>
2025-12-27 01:35:01 +0200

    fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import HostGameSetup from './pages/HostSetup';
 import MobileController from './pages/MobileController';
