81377140207c1d79d928b3aaadd65696732d04d4
YARINSWINDOWSPC\yarin <yarinmagdaci@gmail.com>
2025-12-27 01:23:38 +0200

    fixes github pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import HostGameSetup from './pages/HostSetup';
 import MobileController from './pages/MobileController';
@@ -15,16 +15,16 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path="/" element={<HostGameSetup />} />
+          <Route path="/" element={<Navigate to="/room" replace />} />
           <Route path="/room" element={<HostGameSetup />} />
           <Route path="/game/:hostId?" element={<Index />} /> {/* Using Index temporarily as GameScreen isn't exported as page yet, will fix */}
           <Route path="/controller/:hostId" element={<MobileController />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
